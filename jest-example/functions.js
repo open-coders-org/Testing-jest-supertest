5cfcf976c1ca28586b00fe2936c0319e38185c32
Edilson <eddylson.londonio@gmail.com>
2020-12-29 23:44:37 -0500

    example testing with objects

--- jest-example/functions.js
+++ jest-example/functions.js
@@ -4,5 +4,16 @@ function fibo (n){
 	else return fibo(n -1) + fibo( n - 2 )
 }
 
+function countWords(string){
+	const words = string.split(' ')
+	const count = words.reduce((counter, word) => {
+		counter[word] = !counter[word] ? 1 : counter[word] + 1
+		return counter
+	},{})
+	return count
+}
 
-module.exports = fibo
+module.exports = {
+	fibo,
+	countWords
+}
--- jest-example/test/functions.test.js
+++ jest-example/test/functions.test.js
@@ -1,6 +1,16 @@
-const fibo = require('../functions')
+const { fibo , countWords} = require('../functions')
 
 test('fibo 8 is 21 ', () => {
 	expect(fibo(8)).toBe(21)
 
+})
+
+test('counting words',() => {
+	const words = 'test objects with with jest'
+	expect(countWords(words)).toEqual({
+		test:1,
+		objects:1,
+		with:2,
+		jest:1
+	})
 })
